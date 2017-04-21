af447ffb1856173ca907b063017192f26cf82389
Steven Silvester <steven.silvester@ieee.org>
2017-04-21 08:04:22 -0500

    wip integrating with jupyterlab

--- jupyterlab_launcher/app.py
+++ jupyterlab_launcher/app.py
@@ -18,15 +18,23 @@ class LabLauncherApp(NotebookApp):
     default_url = Unicode('/lab',
         help="The default URL to redirect to from `/`")
 
+    lab_page_title = Unicode('JupyterLab',
+        help="The title of the lab page")
+
+    lab_config_dir = Unicode(ENV_CONFIG_PATH[0],
+        help="The config directory of the lab")
+
     def start(self):
-        config_dir = ENV_CONFIG_PATH[0]
+        config_dir = self.lab_config_dir
         page_file = os.path.join(config_dir, 'page_config.json')
-        with open(page_file) as fid:
-            page_config = json.load(fid)
         build_file = os.path.join(config_dir, 'build_config.json')
         with open(build_file) as fid:
             build_config = json.load(fid)
-        add_handlers(self.web_app, page_config, build_config, self.default_url)
+        build_dir = os.path.join(build_config['location'], 'build')
+        add_handlers(
+            self.web_app, page_file, build_dir, self.lab_page_title,
+            self.default_url
+        )
         super.start()
 
 
--- jupyterlab_launcher/handlers.py
+++ jupyterlab_launcher/handlers.py
@@ -3,6 +3,7 @@
 
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+import json
 import os
 from tornado import web
 
@@ -22,14 +23,28 @@ FILE_LOADER = FileSystemLoader(HERE)
 class LabHandler(IPythonHandler):
     """Render the JupyterLab View."""
 
-    def initialize(self, page_title, css_files, bundle_files, page_config):
+    def initialize(self, page_title, css_files, bundle_files, page_config_file):
         self.page_title = page_title
         self.css_files = css_files
         self.bundle_files = bundle_files
-        self.page_config = page_config
+        self.page_config_file = page_config_file
 
     @web.authenticated
     def get(self):
+        # Handle page config data.
+        page_config = dict()
+        page_config.update(self.settings.get('page_config_data', {}))
+        terminals = self.settings.get('terminals_available', False)
+        page_config.setdefault('terminalsAvailable', terminals)
+        page_config.setdefault('ignorePlugins', [])
+
+        if os.path.exists(self.page_config_file):
+            with open(self.page_config_file) as fid:
+                try:
+                    page_config.update(json.load(fid))
+                except Exception as e:
+                    print(e)
+
         mathjax_config = self.settings.get('mathjax_config',
                                            'TeX-AMS_HTML-full,Safe')
         config = dict(
@@ -46,37 +61,29 @@ class LabHandler(IPythonHandler):
         return FILE_LOADER.load(self.settings['jinja2_env'], name)
 
 
-def add_handlers(web_app, page_config, build_config, prefix):
+def add_handlers(web_app, page_config_file, build_dir, page_title, prefix):
     """Add the appropriate handlers to the web app.
     """
     base_url = web_app.settings['base_url']
     prefix = ujoin(base_url, prefix)
-    location = build_config['location']
-    page_title = build_config.get('page_title', 'JupyterLab')
-
-    # Handle page config data.
-    page_config.update(web_app.settings.get('page_config_data', {}))
-    terminals = web_app.settings.get('terminals_available', False)
-    page_config.setdefault('terminalsAvailable', terminals)
-    page_config.setdefault('ignorePlugins', [])
 
     bundle_files = []
     css_files = []
     for entry in ['main']:
         css_file = entry + '.css'
-        if os.path.isfile(os.path.join(location, 'build', css_file)):
+        if os.path.isfile(os.path.join(build_dir, css_file)):
             css_files.append(ujoin(prefix, css_file))
         bundle_files.append(ujoin(prefix, entry + '.bundle.js'))
 
     handlers = [
         (prefix + r'/?', LabHandler, {
-            'page_config': page_config,
+            'page_config_file': page_config_file,
             'css_files': css_files,
             'bundle_files': bundle_files,
             'page_title': page_title
         }),
         (prefix + r"/(.*)", FileFindHandler, {
-            'path': os.path.join(location, 'build')
+            'path': build_dir
         })
     ]
     web_app.add_handlers(".*$", handlers)
