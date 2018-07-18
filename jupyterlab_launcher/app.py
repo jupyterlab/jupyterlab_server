0eb67e3dc0c053efc7789f9122c976d607ed27a4
Afshin Darian <git@darian.af>
2018-07-18 16:44:56 +0100

    LabConfig should not have been removed here.

--- jupyterlab_launcher/app.py
+++ jupyterlab_launcher/app.py
@@ -6,13 +6,15 @@
 from notebook.notebookapp import NotebookApp
 from traitlets import Unicode
 
-from .handlers import add_handlers
+from .handlers import add_handlers, LabConfig
 
 
 class LabLauncherApp(NotebookApp):
 
     default_url = Unicode('/lab',
                           help='The default URL to redirect to from `/`')
+
+    lab_config = LabConfig()
 
     def start(self):
         add_handlers(self.web_app, self.lab_config)
