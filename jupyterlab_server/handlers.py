8f6cee4ecccc9b1877f5c04db8bf3e199e508c3a
Jason Grout <jasongrout@users.noreply.github.com>
2019-08-15 16:49:49 -0700

    Escape template values in Jinja (#75)
    * Encode URLs in the template
    
    * Encode json values in page config
    
    * Revert "fix page config escaping (#73)"
    
    This reverts commit 0883a38ab5d416c44d3351304a5f169f3585b119.
    
    Escaping is now done in the template as of commit bbff324c9e0a274d50ecbe97b090f0eb16f6e772
    
    * Escape error template values

--- jupyterlab_server/handlers.py
+++ jupyterlab_server/handlers.py
@@ -93,10 +93,6 @@ class LabHandler(JupyterHandler):
             full_url = ujoin(base_url, getattr(config, name))
             page_config[full_name] = full_url
 
-        # JSON escape Python values in page_config
-        for key, value in page_config.items():
-            page_config[key] = json.dumps(str(value))[1:-1]
-
         # Load the current page config file if available.
         page_config_file = os.path.join(settings_dir, 'page_config.json')
         if os.path.exists(page_config_file):
