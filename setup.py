1cc0654e58789299597a38367443713d00361337
Eric Charles <eric@datalayer.io>
2020-07-29 13:13:44 +0200

    Use jserver extension-loadin branch

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ if 'setuptools' in sys.modules:
         'json5',
         'jsonschema>=3.0.1',
         'jinja2>=2.10',
-        'jupyter_server@ git+https://github.com/datalayer-contrib/jupyter-server.git@py35',
+        'jupyter_server@ git+https://github.com/ZSailer/jupyter-server.git@extension-loading',
     ],
     setup_args['entry_points'] = {
         'pytest11': [
