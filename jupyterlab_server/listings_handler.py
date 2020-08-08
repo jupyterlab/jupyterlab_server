a1eadb11aa6a8f03487632dc20253bfe32544b3e
Eric Charles <eric@datalayer.io>
2020-08-08 15:16:51 +0200

    Use blocked/allowed extension naming

--- jupyterlab_server/listings_handler.py
+++ jupyterlab_server/listings_handler.py
@@ -64,9 +64,9 @@ class ListingsHandler(APIHandler):
     blocked_extensions_uris = set()
     # The list of allowed_extensions URIS.
     allowed_extensions_uris = set()
-    # The blocked_ extensions extensions.
+    # The blocked extensions extensions.
     blocked_extensions = []
-    # The allowed_ extensions extensions.
+    # The allowed extensions extensions.
     allowed_extensions = []
     # The provider request options to be used for the request library.
     listings_request_opts = {}
