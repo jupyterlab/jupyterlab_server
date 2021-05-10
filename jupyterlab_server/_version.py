4beb9089a070320ed23c13073aabdeb3ba3c6c4a
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-05-10 12:02:49 +0200

    Remove Packaging Dependency

--- jupyterlab_server/_version.py
+++ jupyterlab_server/_version.py
@@ -1,3 +1,15 @@
-from jupyter_packaging import get_version_info
+"""
+store the current version info of the server.
+
+"""
+import re
+
 __version__ = '2.5.0'
-version_info = get_version_info(__version__)
+
+# Build up version_info tuple for backwards compatibility
+pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
+match = re.match(pattern, __version__)
+parts = [int(match[part]) for part in ['major', 'minor', 'patch']]
+if match['rest']:
+  parts.append(match['rest'])
+version_info = tuple(parts)
