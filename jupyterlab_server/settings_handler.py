5d66489d6953791903f9ae892674a4c0c46337f4
telamonian <telamonian@users.noreply.github.com>
2021-09-07 13:36:21 -0400

    `get_current_locale`: fall back to `DEFAULT_LOCALE` when translation settings schema is not valid
    - needed to fix final CI bug for jupyterlab/jupyterlab#10929

--- jupyterlab_server/settings_handler.py
+++ jupyterlab_server/settings_handler.py
@@ -35,7 +35,14 @@ class SettingsHandler(ExtensionHandlerMixin, ExtensionHandlerJinjaMixin, SchemaH
         """Get setting(s)"""
         # Need to be update here as translator locale is not change when a new locale is put
         # from frontend
-        translator.set_locale(self.get_current_locale())
+        try:
+            locale = self.get_current_locale()
+        except web.HTTPError as e:
+            # fallback in case of missing (404) or misshapen (500) translation schema
+            locale = DEFAULT_LOCALE
+            'Failed loading or validating translation settings schema'
+
+        translator.set_locale(locale)
 
         result, warnings = get_settings(
             self.app_settings_dir,
