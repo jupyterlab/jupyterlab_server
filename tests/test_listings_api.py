6b5e050155b97940ea96de93309c261fe42ebfb9
Frédéric Collonval <fcollonval@users.noreply.github.com>
2022-07-28 14:58:52 +0200

    Remove tests

--- tests/test_listings_api.py
+++
@@ -1,7 +0,0 @@
-from jupyterlab_server.test_utils import validate_request
-
-
-async def test_get_listing(jp_fetch, labserverapp):
-    url = r"lab/api/listings/@jupyterlab/extensionmanager-extension/listings.json"
-    r = await jp_fetch(*url.split("/"))
-    validate_request(r)
