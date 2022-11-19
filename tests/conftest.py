588782914459c9f96f8a8bdccc917242700a375f
Steven Silvester <steven.silvester@ieee.org>
2022-11-19 13:14:48 -0600

    ignore warnings on pypy

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,10 @@
 import os
+import sys
 
 os.environ["JUPYTER_PLATFORM_DIRS"] = "1"
 pytest_plugins = ["jupyterlab_server.pytest_plugin"]
+
+if sys.implementation.lower() == "pypy":
+    import warnings
+
+    warnings.filterwarnings("ignore")
