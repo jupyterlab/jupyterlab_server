96283dec03b0f1dcab23aa5f8f37177da4e0d2a4
Steven Silvester <steven.silvester@ieee.org>
2022-11-19 13:21:28 -0600

    try again

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import sys
 os.environ["JUPYTER_PLATFORM_DIRS"] = "1"
 pytest_plugins = ["jupyterlab_server.pytest_plugin"]
 
-if sys.implementation.lower() == "pypy":
+if sys.implementation.name.lower() == "pypy":
     import warnings
 
     warnings.filterwarnings("ignore")
