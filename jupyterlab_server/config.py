9536a91ba97176804999b6020717bf85fd53c696
Steven Silvester <steven.silvester@ieee.org>
2024-03-02 07:21:25 -0600

    Use json5 typings

--- jupyterlab_server/config.py
+++ jupyterlab_server/config.py
@@ -12,7 +12,7 @@ from logging import Logger
 from os.path import join as pjoin
 from typing import Any
 
-import json5  # type:ignore[import-untyped]
+import json5
 from jupyter_core.paths import SYSTEM_CONFIG_PATH, jupyter_config_dir, jupyter_path
 from jupyter_server.services.config.manager import ConfigManager, recursive_update
 from jupyter_server.utils import url_path_join as ujoin
