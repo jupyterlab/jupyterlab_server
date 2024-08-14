01971335995ce2082a2fb366ac58b7750c72ed70
Anastasiia Sliusar <anastasiia.sliusar@gmail.com>
2024-08-14 14:29:51 +0200

    Fix linting

--- jupyterlab_server/handlers.py
+++ jupyterlab_server/handlers.py
@@ -83,7 +83,9 @@ class LabHandler(ExtensionHandlerJinjaMixin, ExtensionHandlerMixin, JupyterHandl
         server_root = server_root.replace(os.sep, "/")
         base_url = self.settings.get("base_url")
         server_app = self.settings.get("serverapp")
-        allow_insecure_kernelspec_params = server_app.allow_insecure_kernelspec_params
+        assert server_app is not None
+        if server_app.allow_insecure_kernelspec_params:
+            allow_insecure_kernelspec_params = server_app.allow_insecure_kernelspec_params
 
         # Remove the trailing slash for compatibility with html-webpack-plugin.
         full_static_url = self.static_url_prefix.rstrip("/")
