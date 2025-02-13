ec3cdaa1443e09c80bbdad729b8f373ea47bc2b3
SHRISHTISHUKLA-0 <shrishtishukla01332@gmail.com>
2025-02-13 10:26:12 +0530

    Fixed import issue for _camelCase in handlers.py

---
+++ jupyterlab_server/utils.py
@@ -0,0 +1,4 @@
+def _camelCase(snake_str: str) -> str:
+    """Convert snake_case string to camelCase."""
+    components = snake_str.split('_')
+    return components[0] + ''.join(x.title() for x in components[1:])
